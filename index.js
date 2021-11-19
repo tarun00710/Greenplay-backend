26ea362c45e7759bab49728c91971cf0d468c035
tarun00710 <tarunlal07@gmail.com>
2021-11-19 11:28:56 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express =require('express');
 const cors=require('cors')
 const app=express();
-const bodyParser = require('body-parser')
+// const bodyParser = require('body-parser')
 const {connectionDB}=require('./DB/mongoDB')
 
 const PORT=process.env.PORT || 5000;
